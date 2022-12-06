94f0c1822bc0b9e6c02902c9f122727d9d1085ab
NQDark <volodymyr.vysotskyi.thinkglobal@gmail.com>
2022-12-06 14:51:33 +0200

    hw6

--- main.js
+++ main.js
@@ -74,3 +74,29 @@
 //       chelsy: 38,
 //    }),
 // ); // lux
+
+// task 4
+const countTotalSalary = function(employees) {
+    // твой код
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(countTotalSalary({})); // 0
+  
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
+  
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
